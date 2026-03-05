93bf9cd09150573dcddd4b964b88206d2471267e
MaessaAndreaVallenia <maessaandrea.2023@student.uny.ac.id>
2026-03-05 14:01:38 +0700

    Menambahkan Praktikum Pertemuan 04

--- Pertemuan04_23051430044_MaessaAndreaVallenia/praktikum.js
+++ Pertemuan04_23051430044_MaessaAndreaVallenia/praktikum.js
@@ -1,15 +1,19 @@
 // Komentar single line
+
 // 1. Variabel & Tipe Data
 let namaMesin = "CNC-Mazak-01"; // String
 let targetHarian = 500; // Number
 // Menampilkan ke console browser (Tekan F12 -> Console)
 console.log("Mesin: " + namaMesin);
 console.log("Target: " + targetHarian);
+
 // 2. Operator Aritmatika
 let produksiPagi = 200;
 let produksiSiang = 150;
 let totalProduksi = produksiPagi + produksiSiang;
+
 console.log("Total saat ini: " + totalProduksi);
+
 // Hitung sisa kekurangan target
 let kekurangan = targetHarian - totalProduksi;
 console.log("Kekurangan target: " + kekurangan);
@@ -17,7 +21,9 @@ console.log("Kekurangan target: " + kekurangan);
 // Simulasi data pembacaan sensor (jam operasional)
 let jamOperasional = 1250; // dalam jam
 let batasMaksimal = 1200; // batas sebelum maintenance wajib
+
 console.log("--- Cek Status Maintenance ---");
+
 // Logika If/Else
 if (jamOperasional >= batasMaksimal) {
     console.log("PERINGATAN: Mesin mencapai batas maksimal!");
@@ -31,13 +37,17 @@ if (jamOperasional >= batasMaksimal) {
 // Data Input
 let jamKerjaPlanned = 8; // Jam
 let jamKerjaAktual = 6.5; // Jam (Ada 1.5 jam breakdown)
+
 // Perhitungan
 let availability = (jamKerjaAktual / jamKerjaPlanned) * 100;
+
 // Pembulatan 2 angka di belakang koma
 availability = availability.toFixed(2);
+
 console.log("Planned Time: " + jamKerjaPlanned + " Jam");
 console.log("Actual Time: " + jamKerjaAktual + " Jam");
 console.log("Availability: " + availability + "%");
+
 // Logika Penilaian Kualitas Availability
 if (availability >= 90) {
     console.log("Kategori: WORLD CLASS");
@@ -49,6 +59,7 @@ if (availability >= 90) {
 
 let namaOperator = prompt("Masukkan Nama Operator:");
 let shiftKerja = prompt("Masukkan Shift (Pagi/Siang/Malam):");
+
 if (shiftKerja === "Malam") {
  alert("Halo " + namaOperator + ", Shift malam memiliki tambahan uang makan sebesar Rp 20.000.");
 } else {
