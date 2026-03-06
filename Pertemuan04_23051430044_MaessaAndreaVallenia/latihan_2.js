dc1a36b31b164faacb7f549c712af1afbf1d276e
MaessaAndreaVallenia <maessaandrea.2023@student.uny.ac.id>
2026-03-06 23:45:34 +0700

    Menambahkan Latihan 2 Pertemuan 4

--- Pertemuan04_23051430044_MaessaAndreaVallenia/latihan_2.js
+++ Pertemuan04_23051430044_MaessaAndreaVallenia/latihan_2.js
@@ -1,11 +1,17 @@
-let input = Number(prompt("Masukkan kode shift (1=Pagi, 2=Siang, 3=Malam)"));
-
 if (input === 1) {
-    console.log("Shift Anda adalah Pagi");
+    outputDiv.textContent = "Shift Anda adalah Pagi";
+    outputDiv.style.color = "green";
+    console.log("Shift Anda adalah Pagi"); // ini supaya muncul di console juga
 } else if (input === 2) {
+    outputDiv.textContent = "Shift Anda adalah Siang";
+    outputDiv.style.color = "green";
     console.log("Shift Anda adalah Siang");
 } else if (input === 3) {
+    outputDiv.textContent = "Shift Anda adalah Malam";
+    outputDiv.style.color = "green";
     console.log("Shift Anda adalah Malam");
 } else {
+    outputDiv.textContent = "Shift Tidak Valid";
+    outputDiv.style.color = "red";
     console.log("Shift Tidak Valid");
 }
