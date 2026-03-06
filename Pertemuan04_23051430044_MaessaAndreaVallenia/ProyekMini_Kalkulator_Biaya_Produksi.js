e615a501e5245b32451ccfc9a635fadb7fb94103
MaessaAndreaVallenia <maessaandrea.2023@student.uny.ac.id>
2026-03-06 07:56:52 +0700

    Menambahkan Proyek Mini Pertemuan 04

--- Pertemuan04_23051430044_MaessaAndreaVallenia/ProyekMini_Kalkulator_Biaya_Produksi.js
+++ Pertemuan04_23051430044_MaessaAndreaVallenia/ProyekMini_Kalkulator_Biaya_Produksi.js
@@ -1,25 +1,24 @@
 document.getElementById("formProduksi").addEventListener("submit", function (event) {
 
     event.preventDefault();
-    // 1. Inisialisasi data biaya produksi
+    // Field input
     let biayaBahanBaku = Number(document.getElementById("bahanBaku").value);
     let biayaTenagaKerja = Number(document.getElementById("tenagaKerja").value);
     let biayaOverhead = Number(document.getElementById("overhead").value);
-    let totalProduksi = Number(document.getElementById("jumlahProduksi").value);
+    let jumlahProduksi = Number(document.getElementById("jumlahProduksi").value);
 
 
-    // 2. Validasi input jumlah produksi agar tidak negatif atau nol
-    if (totalProduksi <= 0) {
+    // Validasi agar jumlah nya tidak negatif
+    if (jumlahProduksi <= 0) {
         output.textContent = "Jumlah produksi tidak valid.";
         console.log("Jumlah produksi tidak valid.");
         return;
-    }
-
-    // 3. Perhitungan biaya total per unit produksi
+    } 
+    // Perhitungan biaya total perunit
     let totalPerUnit =
         (biayaBahanBaku + biayaTenagaKerja + biayaOverhead) / jumlahProduksi;
 
-    // 4. Penentuan status efisiensi biaya berdasarkan jumlah produksi
+    // logika if else untuk mengecek jumlah produksi
     let status;
     if (jumlahProduksi < 100) {
         status = "Biaya Tinggi (Ekonomi Skala Kecil)";
@@ -27,13 +26,14 @@ document.getElementById("formProduksi").addEventListener("submit", function (eve
         status = "Biaya Efisien";
     }
 
-    // 5. Menampilkan hasil perhitungan pada halaman web
+    // Field output hasil dari pemrosesan
     let output = document.getElementById("hasilOutput");
+    // Untuk menampilkan di website
     output.innerHTML =
         "Total Biaya per Unit: Rp " + totalPerUnit.toLocaleString("id-ID", { minimumFractionDigits: 2 }) +
         "<br>Status Produksi: " + status;
 
-    // 6. Menampilkan hasil perhitungan pada console untuk dokumentasi
+    // Untuk menampilkan di console
     console.log("===== HASIL PERHITUNGAN =====");
     console.log("Biaya Bahan Baku: Rp", biayaBahanBaku.toLocaleString("id-ID"));
     console.log("Biaya Tenaga Kerja: Rp", biayaTenagaKerja.toLocaleString("id-ID"));
